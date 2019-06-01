cfe49b461e46a292233a096591c3482dcee0e043
Alice Liu <alice.xyl@gmail.com>
2019-05-31 17:16:27 -0700

    Added making hacker news external API call

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const stories = require('./stories');
 
 const app = express();
@@ -27,6 +28,14 @@ app.get('/stories/:title', (req, res) => {
     const { title } = req.params;
 
     res.json(stories.filter(story => story.title.includes(title)));
+});
+
+app.get('/topstories', (req, res) => {
+    request(
+        { url: 'https://hacker-news.firebaseio.com/v0/topstories.json'},
+        (error, response, body) => {
+            res.json(JSON.parse(body));
+        });
 });
 
 const PORT = 3000;
