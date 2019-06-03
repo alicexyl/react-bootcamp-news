99b779a2d03a54dd8c15bd0e977ef5932eb6bc9c
Alice Liu <alice.xyl@gmail.com>
2019-06-03 15:52:51 -0700

    Display latest Hacker News

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
+const SERVER_ROOT = 'http://localhost:3000';
+
 class App extends Component {
+    state = { stories: [] };
+
+    componentDidMount() {
+        fetch(`${SERVER_ROOT}/topstories`)
+        .then(response => response.json())
+        .then(json => this.setState({ stories: json }))
+        .catch(error => alert(error.message));
+    }
+
    render() {
         return (
             <div>
-                News
+                <h2>Latest Hacker News</h2>
+                <div>
+                    { 
+                        this.state.stories.map(story => {
+                            const { id, title, by, score, url, time } = story;
+
+                            return (
+                                <div key={id}>
+                                    <a href={url}><h3>{title}</h3></a>
+                                    <p>Upvotes: {score}</p>
+                                    <p>{by} - {new Date(time).toLocaleString()}</p>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
             </div>
         );
     }
